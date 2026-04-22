d2d8e8f771d785b81bdbb5bd28c9862823a26b91
yuanwu1454 <yuanwu1454@users.noreply.github.com>
2026-04-22 21:18:40 +0800

    add

--- Source/MyGame/Anim/KingHeroAnimInstance.cpp
+++ Source/MyGame/Anim/KingHeroAnimInstance.cpp
@@ -2,3 +2,29 @@
 
 
 #include "KingHeroAnimInstance.h"
+
+#include "King/KingCharacter.h"
+
+void UKingHeroAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	if (OwningCharacter)
+	{
+		OwningKingCharacter = Cast<AKingCharacter>(OwningCharacter);
+	}
+}
+
+void UKingHeroAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	if (bHasAcceleration)
+	{
+		IdleElpasedTime = 0.f;
+		bShouldEnterRelaxState = false;
+	}else
+	{
+
+		IdleElpasedTime += DeltaSeconds;
+		bShouldEnterRelaxState = (IdleElpasedTime>=EnterRelaxStateThreshold);
+	}
+}
--- Source/MyGame/Anim/KingHeroAnimInstance.h
+++ Source/MyGame/Anim/KingHeroAnimInstance.h
@@ -6,6 +6,7 @@
 #include "KingCharacterAnimInstance.h"
 #include "KingHeroAnimInstance.generated.h"
 
+class AKingCharacter;
 /**
  * 
  */
@@ -13,4 +14,19 @@ UCLASS()
 class MYGAME_API UKingHeroAnimInstance : public UKingCharacterAnimInstance
 {
 	GENERATED_BODY()
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="AnimData|Reference")
+	AKingCharacter* OwningKingCharacter;
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="AnimData|LocomotionData")
+	bool bShouldEnterRelaxState;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="AnimData|LocomotionData")
+	float EnterRelaxStateThreshold =5.f;
+
+	float IdleElpasedTime;
+	
 };
+
+
